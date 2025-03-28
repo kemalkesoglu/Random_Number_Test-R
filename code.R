4257d2cc3d91c0ad49a4ab8adea0cca1b3e90dd5
Kemal KEŞOĞLU <kemalkesoglu@gmail.com>
2025-03-28 16:27:13 +0300

    code.R

--- code.R
+++ code.R
@@ -1,4 +1,4 @@
-#Fonksiyonu olusturalim.
+#Fonksiyonu istenilen sekilde olusturalim.
 lcg <- function(a, c, m, run.length, seed) {
   x <- rep(0, run.length)
   x[1] <- seed
@@ -10,15 +10,14 @@ lcg <- function(a, c, m, run.length, seed) {
 }
 
 
-#NOT:Her çalıştırıldığında değerler değişir.
-#başlangıç seed değeri olarak sistem zamanını kullanılmıştır.
-
-
-
 #Fonksiyon ile 100 tane ve 10000 tane rastgele veri üretelim.
 set.seed(Sys.time())  
 z <- lcg(30, 90, 150, 100, sample(1:150, 1))  
 z2 <- lcg(30, 90, 150, 10000, sample(1:150, 1))  
+
+
+#NOT:Her çalıştırıldığında oluşan veriler değişir.
+#başlangıç seed değeri olarak sistem zamanını kullanılmıştır.
 
 
 
@@ -38,14 +37,14 @@ z2
 #Düsük bir p degeri dizinin test edilen gecikmelerdeki degerler arasinda bagimliliklara sahip oldugunu gösterir.
 Box.test (z$x, lag = 10, type = "Ljung")
 Box.test (z$U, lag = 10, type = "Ljung")
-#Uygulanan Ljung-Box Test sonucunda P=0.75 oldugundan önemli bir otokorelasyon olmadigini söyleyebiliriz.
+#Uygulanan Ljung-Box Test sonucunda P=0.8464 oldugundan önemli bir otokorelasyon olmadigini söyleyebiliriz.
 
 #Ljung-Box test uygulayalim.
 #Yüksek bir p degeri önemli bir otokorelasyon olmadigini gösterir.
 #Düsük bir p degeri dizinin test edilen gecikmelerdeki degerler arasinda bagimliliklara sahip oldugunu gösterir.
 Box.test (z2$x, lag = 10, type = "Ljung")
 Box.test (z2$U, lag = 10, type = "Ljung")
-#Uygulanan Ljung-Box Test sonucunda P=0.39 oldugundan önemli bir otokorelasyon olmadigini söyleyebiliriz.
+#Uygulanan Ljung-Box Test sonucunda P=0.6824 oldugundan önemli bir otokorelasyon olmadigini söyleyebiliriz.
 
 
 
@@ -54,14 +53,14 @@ observed_z <- table(cut(z$U, breaks=8))
 expected_z <- rep(length(z$U) / 8, 8)  
 chisq_test_z <- chisq.test(observed_z, p=expected_z / sum(expected_z), rescale.p=TRUE)
 chisq_test_z
-#P = 0.36, anlamlilik degeri a = 0.05 den büyük oldugundan rastgele olusturdugumuz
+#P = 0.2673, anlamlilik degeri a = 0.05 den büyük oldugundan rastgele olusturdugumuz
 #verilerin dagilimi ile belirtilen dagilim arasinda fark olmadigini söyleyebiliriz.
 
 observed_z2 <- table(cut(z2$U, breaks=15))  
 expected_z2 <- rep(length(z2$U) / 15, 15)  
 chisq_test_z2 <- chisq.test(observed_z2, p=expected_z2 / sum(expected_z2), rescale.p=TRUE)
 chisq_test_z2
-#P = 0.73, anlamlilik degeri a = 0.05 den büyük oldugundan rastgele olusturdugumuz
+#P = 0.8465, anlamlilik degeri a = 0.05 den büyük oldugundan rastgele olusturdugumuz
 #verilerin dagilimi ile belirtilen dagilim arasinda fark olmadigini söyleyebiliriz.
 
 
@@ -70,12 +69,12 @@ chisq_test_z2
 library(randtoolbox)
 serial_test_z <- serial.test(z$U, d = 2, echo = TRUE)
 serial_test_z
-#Uygulanan serial test sonucunda P=0.54, a = 0.05 den büyük oldugu için H0 Reddedilemez.
+#Uygulanan serial test sonucunda P=0.19, a = 0.05 den büyük oldugu için H0 Reddedilemez.
 #verilerin random oldugu söylenebilir.
 
 serial_test_z2 <- serial.test(z2$U, d = 2, echo = TRUE)
 serial_test_z2
-#Uygulanan serial test sonucunda P=0.71, a = 0.05 den büyük oldugu için H0 Reddedilemez.
+#Uygulanan serial test sonucunda P=0.74, a = 0.05 den büyük oldugu için H0 Reddedilemez.
 #verilerin random oldugu söylenebilir.
 
 
@@ -97,23 +96,22 @@ if(!require(randtests)) install.packages("randtests")
 
 runs_test_z_x <- runs.test(z$x)
 runs_test_z_x
-#P degeri 0.84 oldugu için verilerin random oldugu söylenebilir.
+#P degeri 0.5 oldugu için verilerin random oldugu söylenebilir.
 
 runs_test_z_U <- runs.test(z$U)
 runs_test_z_U
-#P degeri 0.84 oldugu için verilerin random oldugu söylenebilir.
+#P degeri 0.5 oldugu için verilerin random oldugu söylenebilir.
 
 runs_test_z2_x <- runs.test(z2$x)
 runs_test_z2_x
-#P degeri 0.23 oldugu için verilerin random oldugu söylenebilir.
+#P degeri 0.7 oldugu için verilerin random oldugu söylenebilir.
 
 runs_test_z2_U <- runs.test(z2$U)
 runs_test_z2_U
-#P degeri 0.23 oldugu için verilerin random oldugu söylenebilir.
+#P degeri 0.7 oldugu için verilerin random oldugu söylenebilir.
 
 
 
-#Regresyon Modelinde
 #Durbin-Watson testi ile otokorelasyon kontrolü yapalim.
 #DW = 2 ise otokarelasyon yoktur.
 #DW < 2 ise negatif bir otokarelasyon vardir.
@@ -123,14 +121,14 @@ if(!require(lmtest)) install.packages("lmtest")
 lm_model_z <- lm(z$x ~ seq_along(z$x))  
 dw_test_z <- dwtest(lm_model_z)  
 dw_test_z
-#DW=1.97 yani DW ≈ 2 oldugu için otokarelasyon yoktur diyebiliriz.
+#DW=2 oldugu için otokarelasyon yoktur diyebiliriz.
 
 lm_model_z2 <- lm(z2$x ~ seq_along(z2$x))  
 dw_test_z2 <- dwtest(lm_model_z2)  
 dw_test_z2
-#DW=2.02 yani DW ≈ 2 oldugu için otokarelasyon yoktur diyebiliriz.
+#DW=2 oldugu için otokarelasyon yoktur diyebiliriz.
 
 
 
-#Testlerimiz ve görselleştirmelerimiz sonucunda üretilen verilerin otokorelasyonlarının olmadığını
-#Random olduğunu söyleyebiliriz.
+#Testlerimiz ve görsellestirmelerimiz sonucunda üretilen verilerin otokorelasyonlarinin olmadigini
+#Random oldugunu söyleyebiliriz.
